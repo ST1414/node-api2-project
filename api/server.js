acf3f52c20f51f7bffda34679c1e06f6576f23c7
Sean Terry <seanterry@Seans-MBP.localdomain>
2021-11-30 13:06:55 -0800

    Index and Server files create and successfully running

--- api/server.js
+++ api/server.js
@@ -1,2 +1,16 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require('express');
+const postsRouter = require('./posts/posts-router');
+
+const server = express(); // Instance of the express server
+
+server.use(express.json()); // Tell the server how to parse JSON
+
+server.get('/', (req,res) => {
+    res.send(`
+    <h2> Server is up and you can see me!</h2>
+    `)
+});
+
+module.exports = server;
