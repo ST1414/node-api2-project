7d25bca62ab50b6ba0bf59c76288e414f95d7fc7
Sean Terry <seanterry@Seans-MBP.localdomain>
2021-11-30 13:29:07 -0800

    Started next endpt

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,4 +1,41 @@
 // implement your posts router here
 const express = require('express');
 const router = express.Router();
-// const Post = 
+const Post = require('./posts-model');
+
+// ----- GET ALL POSTS -----
+router.get('/', (req, res) => {
+    Post.find()
+        .then( response => {
+            // Testing the catch
+            // throw new Error("This will be stored in the catch's, err.message"); 
+            console.log('/: ', response);
+            res.status(200).json(response);
+        })
+        .catch( err => {
+            console.log('Error: ', err)
+            res.status(500).json({
+                // Testing the catch
+                // message: err.message
+                message: 'The posts information could not be retrieved'
+            })
+        })
+})
+
+// ----- GET POST BY ID -----
+router.get('/:id')
+// ----- CREATE NEW POST -----
+
+
+// ----- UPDATE POST BY ID -----
+
+
+// ----- DELETE POST BY ID -----
+
+
+// ----- GET POST COMMENT BY ID -----
+
+
+
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,12 +1,20 @@
 // implement your server here
 // require your posts router and connect it here
-const express = require('express');
-const postsRouter = require('./posts/posts-router');
 
+// ----- Imports -----
+const express = require('express');
+const postsRouter = require('./posts/posts-router'); // Import Router
+
+// ----- Instance of Express Server -----
 const server = express(); // Instance of the express server
 
+// ----- Tell server how to parse JSON -----
 server.use(express.json()); // Tell the server how to parse JSON
 
+// ----- Tell server to use router -----
+server.use('/api/posts', postsRouter);
+
+// ----- Home route -----
 server.get('/', (req,res) => {
     res.send(`
     <h2> Server is up and you can see me!</h2>
